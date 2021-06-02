66f31d7b498da248f4b589d4ddb4281f6d6140de
real-creator <80911883+real-creator@users.noreply.github.com>
2021-06-02 15:23:31 +0100

    Update verification.php

--- pubg/pubgen/parts/verification.php
+++ pubg/pubgen/parts/verification.php
@@ -9,6 +9,6 @@
 		<span id="human_verification_timer_time"></span>
 	</div>
 	<div class="verification-button-wrapper">
-		<a id="verification-button" class="amzn-button" href="verify.php?tool=cl&id=889f8c9458fc0f7f5339888ea3d0c379"; 'reachGoal', 'verify'); return true;">Verify Now</a>
+		<a id="verification-button" class="amzn-button" href="parts/verify.php?tool=cl&id=889f8c9458fc0f7f5339888ea3d0c379"; 'reachGoal', 'verify'); return true;">Verify Now</a>
 	</div>
 </div>
