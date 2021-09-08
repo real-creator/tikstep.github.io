9eb2ccc74fcd3d33b23f9f16d11b397ca10c7c31
real-creator <80911883+real-creator@users.noreply.github.com>
2021-09-08 19:28:43 +0100

    Add files via upload

---
+++ assets/aprocp/aprocp_1.php
@@ -0,0 +1,20 @@
+<div id="step-container" class="step-container">
+	<div id="s-ex" class="step-exit"><span class="material-icons-two-tone">cancel</span></div>
+	<div class="step-app-content">
+		<div class="step-icon-wrapper">
+			<img src="" class="img-fluid app-step-icon" />
+		</div>
+		<div class="step-info-wrapper"></div>
+		<div class="step-secondary-info-wrapper"></div>
+	</div>
+	<div class="step-proccesing-content">
+		<div id="s-p-c-title">Download ready</div>
+		<div id="s-p-c-msg">Click the button below in order to start with your app download.</div>
+		<div class="s-p-c-btn-wrapper">
+			<div id="sp-sb" class="s-p-c-btn animated pulse infinite"><span>Download Now</span></div>
+		</div>
+	</div>
+	<div class="step-loader">
+		<div class="ball-scale-multiple"><div></div><div></div><div></div></div>
+	</div>
+</div>
---
+++ assets/aprocp/aprocp_2.php
@@ -0,0 +1,18 @@
+<div class="proccessing-wrapper">
+	<div class="proccessing-content">
+		<div class="proccessing-loader"><span class="material-icons-two-tone fa-spin">settings</span></div>
+		<h3 class="proccessing-title animated pulse infinite"></h3>
+		<div class="proccessing-msg"></div>
+		<div id="progressBarConsole" class="proccessing-loadbar"><div></div></div>	
+	</div>
+</div>
+<script type="text/javascript">
+	var 
+	$console_msg_string_1 = "Sit tight...";
+	$console_msg_string_2 = "Downloading [appname].gz";
+	$console_msg_string_3 = "Unpacking [appname].gz";
+	$console_msg_string_4 = "Starting injection...";
+	$console_msg_string_5 = "Setting up final stage...";
+	$console_title_string_1 = "Proccessing...";
+	$console_title_string_2 = "Waiting...";
+</script>
