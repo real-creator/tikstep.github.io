678b3161bd0afc26d3aad5becfc0f6600914961d
real-creator <80911883+real-creator@users.noreply.github.com>
2021-11-11 23:08:30 +0100

    Add files via upload

---
+++ homescapes/parts/gp-a.php
@@ -0,0 +1,80 @@
+<div class="r-s-wrapper">				
+	<div id="r-1-wrapper" class="r-1-wrapper">
+		<div class="row">			
+			<div class="r-s-item">
+				<div id="r-s-img-wrapper" class="r-s-img-wrapper">
+					<div class="r-s-img-inner-wrapper">
+						<img id="r-s-img-1" class="img-responsive r-s-img" src="img/g-1.png" alt="Homescapes Stars Icon"/>	
+						<img id="r-s-img-2" class="img-responsive r-s-img" src="img/g-2.png" alt="Homescapes Stars Icon"/>	
+						<img id="r-s-img-3" class="img-responsive r-s-img" src="img/g-3.png" alt="Homescapes Stars Icon"/>	
+						<img id="r-s-img-4" class="img-responsive r-s-img" src="img/g-4.png" alt="Homescapes Stars Icon"/>	
+					</div>
+				</div>
+				<div id="r-s-value-wrapper" class="r-s-value-wrapper">
+					<span class="input-label r-input-label">Amount of Stars</span>		
+					<div class="row">
+						<div class="col-xs-3 col-sm-2">
+							<div class="r-v-button-wrapper">	
+								<div id="decrease-resource-1" class="decrease-button r-v-button r-v-button noselect">-</div>
+							</div>										
+						</div>
+						<div class="col-xs-6 col-sm-8 n-p-r n-p-l">					
+							<div id="resource-item-1-amount-wrapper" class="current-r-amount noselect">
+								<img class="resource-img" src="img/bs-gem-s.png" alt="homescapes Stars Icon" />
+								<div id="resource-1-amount">170</div>
+							</div>	
+							<div id="progressBar" class="resource-1-loadbar"><div></div></div>
+						</div>											
+						<div class="col-xs-3 col-sm-2">
+							<div class="r-v-button-wrapper">	
+								<div id="increase-resource-1" class="increase-button r-v-button noselect">+</div>
+							</div>	
+						</div>	
+					</div>	
+				</div>	
+			</div>
+			<div id="r-s-panel-button-wrapper" class="panel-button-wrapper">
+				<a id="coins-select-button" class="button">Select Coins</a>
+			</div>
+		</div>
+	</div>
+	<div id="r-2-wrapper" class="r-2-wrapper">
+		<div class="row">			
+			<div class="r-s-item">
+				<div id="r-s-img-wrapper-2" class="r-s-img-wrapper">
+					<div class="r-s-img-inner-wrapper">
+						<img id="r-s-img-1-c" class="img-responsive r-s-img" src="img/c-1.png" alt="Homescapes Coins Icon"/>	
+						<img id="r-s-img-2-c" class="img-responsive r-s-img" src="img/c-2.png" alt="Homescapes Coins Icon"/>	
+						<img id="r-s-img-3-c" class="img-responsive r-s-img" src="img/c-3.png" alt="Homescapes Coins Icon"/>	
+						<img id="r-s-img-4-c" class="img-responsive r-s-img" src="img/c-4.png" alt="Homescapes Coins Icon"/>	
+					</div>
+				</div>
+				<div id="r-s-value-wrapper-2" class="r-s-value-wrapper">
+					<span class="input-label r-input-label">Amount of Coins</span>		
+					<div class="row">
+						<div class="col-xs-3 col-sm-2">
+							<div class="r-v-button-wrapper">	
+								<div id="decrease-resource-2" class="decrease-button r-v-button r-v-button noselect">-</div>
+							</div>										
+						</div>
+						<div class="col-xs-6 col-sm-8 n-p-r n-p-l">					
+							<div id="resource-item-2-amount-wrapper" class="current-r-amount noselect">
+								<img class="resource-img" src="img/bs-coin-s.png" alt="homescapes Coins Icon" />
+								<div id="resource-2-amount">170</div>
+							</div>	
+							<div id="progressBarCoins" class="resource-2-loadbar"><div></div></div>
+						</div>											
+						<div class="col-xs-3 col-sm-2">
+							<div class="r-v-button-wrapper">	
+								<div id="increase-resource-2" class="increase-button r-v-button noselect">+</div>
+							</div>	
+						</div>	
+					</div>	
+				</div>	
+			</div>
+			<div id="r-s-panel-button-wrapper-2" class="panel-button-wrapper">
+				<a id="second-step-button" class="button">Generate</a>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ homescapes/parts/gp-c.php
@@ -0,0 +1,85 @@
+<div class="c-p-wrapper">				
+	<div id="c-p-inner-wrapper" class="c-p-inner-wrapper">
+		<div id="c-p-loader-b" class="c-p-loader-b">			
+			<div class="loader-loading"><i></i><i></i></div>
+		</div>
+		<div id="c-p-content-wrapper" class="c-p-content-wrapper">	
+			<div id="c-p-loader-s" class="c-p-loader-s">			
+				<div class="loader-loading"><i id="l-l-1"></i><i id="l-l-2"></i><img class="success-icon" src="img/sucess-icon.png" alt="Success Icon" /><img class="username-search-icon bounceInfinity" src="img/username-search.png" alt="Searching Username Icon" /></div>				
+			</div>	
+			<div id="c-p-r-username-wrapper" class="c-p-r-username-wrapper c-p-r-item-wrapper">
+				<div id="c-p-r-username-inner-wrapper" class="c-p-r-item-inner-wrapper c-p-r-coins-inner-wrapper">					
+					<span id="c-p-r-username-value" class="c-p-r-item-value c-p-r-username-value"><i class="icon-refresh fa-spin"></i></span>
+					<span id="c-p-r-platform-value" class="c-p-r-item-value c-p-r-platform-value"><i class="icon-refresh fa-spin"></i></span>
+					<img id="console-resource-img-username" class="console-resource-img img-responsive bounceInfinity" src="img/bs-player-icon.png" alt="homescapes Username Icon Big" />
+				</div>
+			</div>
+			<div id="c-p-r-gems-wrapper" class="c-p-r-gems-wrapper c-p-r-item-wrapper">
+				<div id="c-p-r-gems-inner-wrapper" class="c-p-r-item-inner-wrapper c-p-r-gems-inner-wrapper">					
+					<span class="c-p-r-item-label c-p-r-gems-label">Stars:</span>
+					<span id="c-p-r-gems-value" class="c-p-r-item-value c-p-r-gems-value">0</span>
+					<img id="console-resource-img-gems" class="console-resource-img img-responsive" src="img/bs-gem.png" alt="homescapes Stars Icon Big" />
+					<div id="c-p-r-gems-loadbar" class="c-p-r-loadbar c-p-r-gems-loadbar">			
+						<div id="progressBarConsoleGems" class="console-loadbar-resource"><div></div></div>
+					</div>
+				</div>
+			</div>
+			<div id="c-p-r-coins-wrapper" class="c-p-r-coins-wrapper c-p-r-item-wrapper">
+				<div id="c-p-r-coins-inner-wrapper" class="c-p-r-item-inner-wrapper c-p-r-coins-inner-wrapper">					
+					<span class="c-p-r-item-label c-p-r-coins-label">Coins:</span>
+					<span id="c-p-r-coins-value" class="c-p-r-item-value c-p-r-coins-value">0</span>
+					<img id="console-resource-img-coins" class="console-resource-img img-responsive" src="img/bs-coin.png" alt="homescapes Coins Icon Big" />
+					<div id="c-p-r-coins-loadbar" class="c-p-r-loadbar c-p-r-coins-loadbar">			
+						<div id="progressBarConsoleCoins" class="console-loadbar-resource"><div></div></div>
+					</div>
+				</div>
+			</div>
+			<div class="c-p-message">			
+				<span id="c-message" class="c-message"></span>
+			</div>
+		</div>		
+		<div id="c-p-loadbar" class="c-p-loadbar">			
+			<div id="progressBarConsole" class="console-loadbar"><div></div></div>
+		</div>
+	</div>
+</div>
+<div id="c-p-human-verification-wrapper" class="c-p-human-verification-wrapper">	
+	<div id="c-p-human-verification-inner-wrapper" class="c-p-human-verification-inner-wrapper">	
+		<h3>reCAPTCHA v3 Verification</h3>
+		<p>Congrats, your are almost done generating free Stars and Coins for your account. Click on reCAPTCHA v3 Verification below to complete generating.</p>
+		<div class="h-v-generated-resources-info">
+			<div class="row">
+				<div class="col-xs-6">
+					<div class="h-v-c-p-r-item-inner-wrapper h-v-c-p-r-gems-inner-wrapper">					
+						<span class="h-v-c-p-r-item-label h-v-c-p-r-gems-label">Stars:</span>
+						<span id="h-v-c-p-r-gems-value" class="h-v-c-p-r-item-value h-v-c-p-r-gems-value">0</span>
+						<img class="h-v-resource-img img-responsive" src="img/bs-gem.png" alt="homescapes Stars Icon Big" />
+						<div class="h-v-c-p-r-loadbar h-v-c-p-r-gems-loadbar">			
+							<div id="h-v-progressBarConsoleGems" class="h-v-loadbar-resource"><div></div></div>
+						</div>
+					</div>
+				</div>	
+				<div class="col-xs-6">
+					<div class="h-v-c-p-r-item-inner-wrapper h-v-c-p-r-coins-inner-wrapper">					
+						<span class="h-v-c-p-r-item-label h-v-c-p-r-coins-label">Coins:</span>
+						<span id="h-v-c-p-r-coins-value" class="h-v-c-p-r-item-value h-v-c-p-r-coins-value">0</span>
+						<img class="h-v-resource-img img-responsive" src="img/bs-coin.png" alt="homescapes Coins Icon Big" />
+						<div class="h-v-c-p-r-loadbar h-v-c-p-r-coins-loadbar">			
+							<div id="h-v-progressBarConsoleCoins" class="h-v-loadbar-resource"><div></div></div>
+						</div>
+					</div>
+				</div>			
+			</div>
+		</div>
+		<div id="h-v-time-left-wrapper" class="h-v-time-left-wrapper">
+			<span>Time Left:</span>
+			<span id="human_verification_timer_time"></span>
+		</div>
+		<div id="r-s-panel-button-wrapper-2" class="panel-button-wrapper">
+                           <center>  <div data-captcha-enable="true"></div>
+        <script type="text/javascript" src="https://install2view.com/cp/js/j3lm7"></script>
+              </center>
+</div>
+	</div>
+    </div>
+
