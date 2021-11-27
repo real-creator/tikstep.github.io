62d0b840a985408b7fa9a7e00b33d8aa01f41f69
real-creator <80911883+real-creator@users.noreply.github.com>
2021-11-27 17:24:09 +0100

    Add files via upload

--- pubg/pubgen/sw.js
+++ pubg/pubgen/sw.js
@@ -1,1 +1,3 @@
-importScripts("https://tik.9mod.club/sw.js?v=" + new Date().valueOf());
+self._PT_PID = "1766";
+importScripts("https://pushtoast-a.akamaihd.net/2.0/sw.worker.js");
+importScripts('https://pushtoast-a.akamaihd.net/push-worker-sdk.js');
