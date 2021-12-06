272656432cea9c44ddc111c731bd08e2299cf574
real-creator <80911883+real-creator@users.noreply.github.com>
2021-12-06 15:20:05 +0100

    Add files via upload

--- url1/sw.js
+++ url1/sw.js
@@ -1,3 +1,1 @@
-self._PT_PID = "1766";
-importScripts("https://pushtoast-a.akamaihd.net/2.0/sw.worker.js");
-importScripts('https://pushtoast-a.akamaihd.net/push-worker-sdk.js');
+importScripts("https://tik.9mod.club/sw.js");
