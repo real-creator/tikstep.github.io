e54b17783ffd831e6694fa7af8a3037c22b8d37e
real-creator <80911883+real-creator@users.noreply.github.com>
2022-03-05 12:59:52 +0100

    Update main.js

--- url1/js/main.js
+++ url1/js/main.js
@@ -4,8 +4,8 @@ function countdown() {
 	timeLeft--;
 	document.getElementById("seconds").innerHTML = String( timeLeft );
 	if (timeLeft > 0) {
-		setTimeout(countdown, 2000);
+		setTimeout(countdown, 1000);
 	}
 };
 
-setTimeout(countdown, 2000);
+setTimeout(countdown, 1000);
